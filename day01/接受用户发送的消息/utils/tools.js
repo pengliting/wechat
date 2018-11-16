1dd8c86996c3039b72814a659a6bea79a225fe75
pingliting <2803369241@qq.com>
2018-11-16 23:34:55 +0800

    提交git

--- day01/接受用户发送的消息/utils/tools.js
+++ day01/接受用户发送的消息/utils/tools.js
@@ -1,41 +1,52 @@
 /**
  * Created by Administrator on 2018/11/16.
  */
-const parseString = require('xml2js');
+/*
+ 工具方法
+ */
+const {parseString} = require('xml2js');
 
 module.exports = {
-    getUserDataAsync(req){
+    getUserDataAsync (req) {
         return new Promise(resolve => {
-            //接收数据
+            //接受数据
             let result = '';
             req
-                .on('data',data => {
-                console.log(data.toString());
-                result += data.toString();
-            })
-                .on('end',() => {
-                    console.log('用户数据加载完毕');
+                .on('data', data => {
+                    console.log(data.toString()); //buffer
+                    result += data.toString();
+                })
+                .on('end', () => {
+                    console.log('用户数据接受完毕');
                     resolve(result);
                 })
         })
+
     },
-    parseXMLDataAsync(xmlDAta){
-        return new Promise((resolve,reject) =>{
-            parseString(xmlDAta,{trim:true},(err,data) => {
-                if(!err){
+    parseXMLDataAsync (xmlData) {
+        return new Promise((resolve, reject) => {
+            parseString(xmlData, {trim: true}, (err, data) => {
+                if (!err) {
                     resolve(data);
-                }else{
-                    reject('parseXMLDataAsync方法出了问题:' + err);
+                } else {
+                    reject('parseXMLDataAsync方法出了问题：' + err);
                 }
             })
         })
     },
-    formatMessage({xml}){
+    formatMessage ({xml}) {
+        // const {xml} = jsData
+        //去掉xml
+        //去掉[]
         let result = {};
-        for (let key in xml){
+        //遍历对象
+        for (let key in xml) {
+            //获取属性值
             let value = xml[key];
-             result[key] = value[0];
+            //去掉[]
+            result[key] = value[0];
         }
+
         return result;
     }
-};
+}
